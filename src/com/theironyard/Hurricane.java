8450235f4e903a140d58d89cc44c208f260edcef
Paige Hetherington <herbmama@gmail.com>
2016-10-14 10:37:22 -0400

    added delete and edit routes

--- src/com/theironyard/Hurricane.java
+++ src/com/theironyard/Hurricane.java
@@ -9,17 +9,21 @@ public class Hurricane {
     enum Category {
         ONE, TWO, THREE, FOUR, FIVE
     }
+    int id;
     String name;
     String location;
     Category category;
     String image;
     User user;
+    boolean isMe;
 
-    public Hurricane(String name, String location, Category category, String image, User user) {
+    public Hurricane(int id, String name, String location, Category category, String image, User user, boolean isMe) {
+        this.id = id;
         this.name = name;
         this.location = location;
         this.category = category;
         this.image = image;
         this.user = user;
+        this.isMe = isMe;
     }
 }
--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -28,6 +28,9 @@ public class Main {
                     if (user != null) {
                         m.put("name", user.name);
                     }
+                    for (Hurricane h : hurricanes) {
+                        h.isMe = user != null && h.user.name.equals(user.name);
+                    }
                     m.put("hurricanes", hurricanes);
                     return new ModelAndView(m, "home.html");
 
@@ -83,13 +86,47 @@ public class Main {
                     String hlocation = request.queryParams("hlocation");
                     Hurricane.Category hcategory = Enum.valueOf(Hurricane.Category.class, request.queryParams("hcategory"));
                     String himage = request.queryParams("himage");
-                    Hurricane h = new Hurricane(hname, hlocation, hcategory, himage, user);
+                    Hurricane h = new Hurricane(hurricanes.size(), hname, hlocation, hcategory, himage, user, false);
                     hurricanes.add(h);
 
                     response.redirect("/");
                     return null;
                 }
         );
+
+        Spark.post(
+                "/delete-hurricane",
+                (request, response) ->  {
+                    Session session = request.session();
+                    String name = session.attribute("loginName");
+                    User user = users.get(name);
+
+                    if (user == null) {
+                        return null;
+                    }
+
+                    int id = Integer.valueOf(request.queryParams("id"));
+
+                    if (hurricanes.get(id) == null || !hurricanes.get(id).isMe) {
+                        return null;
+                    }
+                    hurricanes.remove(id-1);
+                    for (int i = 0; i >hurricanes.size(); i++) {
+                        hurricanes.get(i).id = i;
+
+                    }
+                    response.redirect("/");
+                    return null;
+                }
+        );
+                    Spark.get(
+                            "/edit-hurricane",
+                            (request1, response1) -> {
+                                return  new ModelAndView(null, "/edit.html");
+
+                            }
+                    );
+
 
     }
 }
